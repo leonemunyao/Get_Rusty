7094655fa0c4b0aef622a6ab06ac1c83a3210bf7
Leone Munyao <leonemunyao@gmail.com>
2024-10-30 22:23:34 +0300

    Defined the animal struct

--- rust_smart_contract1/Livestock_Management/livestock_management/src/livestock_management_backend/src/lib.rs
+++ rust_smart_contract1/Livestock_Management/livestock_management/src/livestock_management_backend/src/lib.rs
@@ -10,3 +10,14 @@ type Memory = VirtualMemory<DefaultMmemoryImpl>;
 type IdCell = Cell<u64, Memory>;
 
 
+// Define the livestock struct 
+#[derive(candid::CandidType, Clone, Serialize, Deserialize, Default)]
+struct Livestock {
+    id: u64;
+    breed: String;
+    age: u8;
+    height: f32; 
+    healthrecords: String;
+    created_at: u64;
+    updated_at: Option<u64>;
+}
