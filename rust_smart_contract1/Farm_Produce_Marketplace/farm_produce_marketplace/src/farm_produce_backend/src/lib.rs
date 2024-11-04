454961825db1ac8d52148ab1629bd2107a42025e
Leone Munyao <leonemunyao@gmail.com>
2024-11-04 22:36:57 +0300

    Create function for a seller to update his products. Get function for a buyer to get products based on their names

--- rust_smart_contract1/Farm_Produce_Marketplace/farm_produce_marketplace/src/farm_produce_backend/src/lib.rs
+++ rust_smart_contract1/Farm_Produce_Marketplace/farm_produce_marketplace/src/farm_produce_backend/src/lib.rs
@@ -1,1 +1,58 @@
+// Farm Produce Marketplace Backend.
 
+use ic_cdk::query;
+// import the necessary modules
+use serde::{Deserialize, Serialize};
+use candid::{CandidType, Principal};
+use ic_cdk_macros::{init, update};
+
+
+
+// Define the contract struct
+#[derive(CandidType, Deserialize, Serialize, Clone)]
+pub struct Marketplace {
+    products: Vec<Product>,
+}
+
+// Define the Product struct
+#[derive(CandidType, Deserialize, Serialize, Clone)]
+pub struct Product {
+    id: u64,
+    name: String,
+    description: String,
+    quantity: u64,
+    price: u128,
+    seller: Principal,
+}
+
+#[init]
+pub fn init() {
+    ic_cdk::storage::stable_save((Marketplace { products: Vec::new() },)).unwrap();
+}
+
+#[update]
+pub fn create_product(name: String, description: String, quantity: u64, price: u128) {
+    let (mut marketplace,): (Marketplace,) = ic_cdk::storage::stable_restore().unwrap();
+    let product = Product {
+        id: marketplace.products.len() as u64,
+        name,
+        description,
+        quantity,
+        price,
+        seller: ic_cdk::caller(),
+    };
+    marketplace.products.push(product);
+    ic_cdk::storage::stable_save((marketplace,)).unwrap();
+}
+
+
+// Read function to get all products. Buyers will have the ability to view available products including their current prices and quantities.
+// Read function gets the products by name.
+#[query]
+pub fn get_products(name: String) -> Vec<Product> {
+    let (marketplace,): (Marketplace,) = ic_cdk::storage::stable_restore().unwrap();
+    marketplace.products.into_iter().filter(|product| product.name == name).collect()
+}
+
+// Export the contract
+ic_cdk::export_candid!();
